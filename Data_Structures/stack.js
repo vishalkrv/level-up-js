9331ec4cf27a526605d245cee1c9021f07c739da
Vishal <vishalkrv@gmail.com>
2022-05-18 13:16:34 +0530

    Updated Comments for stacks

--- Data_Structures/stack.js
+++ Data_Structures/stack.js
@@ -1,16 +1,26 @@
+/**
+ * STACK follows last-in-first-out principle (LIFO)
+ *          TIME Complexity                | Space Complexity
+ * +--------------------+---------+-------+-------+
+ * |        Type        | Average | Worst | Worst |
+ * +--------------------+---------+-------+-------+
+ * | Get,Search         | O(n)    | O(n)  | O(n)  |
+ * | Insertion,Deletion | O(1)    | O(1)  | O(n)  |
+ * +--------------------+---------+-------+-------+
+ */
 class Stack {
-  constructor() {
-    this.stack = [];
+  constructor () {
+    this.stack = []
   }
 
-  push(data) {
-    this.stack.push(data);
+  push (data) {
+    this.stack.push(data)
   }
 
-  pop() {
+  pop () {
     //pop takes no argument and automatically removes the top element
-    this.stack.pop();
+    this.stack.pop()
   }
 }
 
-module.exports = Stack;
+module.exports = Stack
